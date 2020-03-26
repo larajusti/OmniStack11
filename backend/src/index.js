67831ae834a1e226af501f8ddd4437813dfa633a
LucasJusti <lucas91js@gmail.com>
2020-03-26 02:13:11 -0300

    Application Back-end

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+
+/* 
+rota/recurso
+*/
+/* 
+GET: buscar informação no back-end
+POST: criar informação no back-end
+PUT: alterar informação no back-end
+*/ 
+/* 
+query params: parametros nomeados enviados na rota apos ?
+servem para filtros/paginação
+route params: utilizados para identificar recursos
+request body: corpo da requisição
+*/
